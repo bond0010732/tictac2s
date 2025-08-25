ee742ba30a777aca172ee869a0a04dcc36878328
bond0010732 <122272180+bond0010732@users.noreply.github.com>
2025-08-25 03:22:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -294,8 +294,9 @@ const notifyAllDevices = async ({ title, message, data }) => {
       to: device.expoPushToken,
       sound: 'default',
       title: title || 'New Join Event 🎮',
-      body: message || `A player has joined room with ₦${amount}. Tap to join the game!`,
-      data: data || {},
+      body: message || `A player has joined room with ₦${room.amount}. Tap to join the game!`,
+      // data: data || {},
+       data: { roomId: room.roomId, amount: room.amount, title: room.title, screen: "StrangersGameScreen" },
     };
 
     console.log("📦 Notification payload:", payload); // 👈 log the payload
